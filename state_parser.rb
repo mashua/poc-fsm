517aa5165a87ddd6ca69ef4de58e11ea62cf4326
Apostolos D. Masiakos <amasiakos@gmail.com>
2017-10-17 10:35:46 +0300

    Implemented c header file creation. Using heredoc sections.

--- state_parser.rb
+++ state_parser.rb
@@ -20,19 +20,76 @@ class StateNode
   end
 end#class def ends here
 
+#The heredoc template for the header file code gen.
+def return_header_file_template(states_array)
+
+ states="";
+ functions="";
+ header_file= <<SECTION1
+#ifndef FSM_H
+#define FSM_H
+
+SECTION1
+ 
+ header_file+= <<SECTION2
+/* The states representation, */
+typedef enum
+{ 
+  NO_STATE = 0,
+SECTION2
+
+ #build the states enum contents
+ states_array.each{ |elem|
+   states+="\t"+elem.to_s;
+   states+=",\n";
+ }
+ header_file+=states;
+ 
+ header_file+= <<SECTION3
+  LAST_STATE
+} theState;
+
+/*MAX_STATES includes the range [NO_STATE - IDLE_STATE] or [0-6]*/
+#define MAX_STATES LAST_STATE-1
+SECTION3
+ 
+ header_file+= <<SECTION4
+
+ /* Function prototypes for triggering states */
+
+SECTION4
+
+ states_array.each{ |elem|
+   functions+="/**\n * Function to trigger state #{elem}.\n * @return\n */\n theState trigger_#{elem}(void);\n\n";
+ }
+ header_file+=functions;
+ header_file+="#endif /*FSM_H*/"; 
+ header_file;
+end
+
 if ARGV.length() == 0 then
   printf("Missing .yml file to parse input from\n");
-  exit(-1);
+  exit(0);
 else
   printf("Using file:#{ARGV[0]} to parse the state graph and produce .c and .h files.\n");
   temp_graph = YAML::load_file(ARGV[0]);
+  
+  states_hash = Hash.new();
+  states_arr = Array.new();
   #temp_graph is an array containing StateNode objects.
   temp_graph.each do |elem|
-    puts elem.to_yaml
+    puts "Now examing #{elem.node_name}...\n";
+    temp_arr = Array.new();
+    elem.nodes_to_go.each do |inner_elem|
+      temp_arr << inner_elem;
+    end
+    states_hash.store("#{elem.node_name}",temp_arr);
+    states_arr<<elem.node_name;
   end
-  #puts temp_graph[1].to_yaml
-
-  exit(-1);
+  puts return_header_file_template(states_arr);
+  
+  puts states_hash;
+  exit(0);
 end
 
 #-----------------------------------------------------------------------
