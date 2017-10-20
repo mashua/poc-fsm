531021a32a930348043f6aabe44e952f354d3829
Apostolos D. Masiakos <amasiakos@gmail.com>
2017-10-20 14:34:06 +0300

    Added a lot of new functionality. See diffs fore more info.

--- state_parser.rb
+++ state_parser.rb
@@ -25,6 +25,11 @@ end#class def ends here
 #The heredoc 'template' for the header file code gen.
 def return_header_file_template(states_array)
 
+#  max_states_len = 0;
+ # states_array.each{ |elem|
+  #  unless elem.length < max_states_len
+  #}
+ 
  states="";
  functions="";
  header_file= <<SECTION1
@@ -34,6 +39,8 @@ def return_header_file_template(states_array)
 SECTION1
  
  header_file+= <<SECTION2
+#define MAX_STATE_LEN = #{6}
+
 /* The states representation, */
 typedef enum
 {
@@ -86,6 +93,7 @@ def return_implementation_file_template(states_array, states_hash)
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "fsm.h"
 
@@ -96,26 +104,39 @@ SECTION1
  * The zero indexed function is 'no_tran' and is used to have one to one
  * mapping between the trigger functions and the array indexes.
  */
-theState (* const state_trigger[MAX_STATES+1])(void) = {\n\tno_tran /* no_tran function used for zero padding */,
+theState (* const state_trigger[MAX_STATES+1])(void) = {\n\tno_tran, /* no_tran function used for zero padding */
 SECTION2
 
  #build the function pointers for the upper array
  states_array.each_index { |elem_index|
-   states+="\t"+states_array[elem_index];
+   states+="\ttrigger_"+"#{states_array[elem_index]}"
    #don't put an extra ',' on the last array element, although that later C standards hava added support for it.
    unless elem_index == states_array.length()-1
      states+=",";
    end
 }
-          
- implementation_file+=states+="\n};\n";
 
+# implementation_file+=states+="\n};\n";
+ states+="\n};\n\n";
+ #build the states name array          
+ states+="static char *state_names[] = {\n\t\"ZP\", /* used for zero padding */\n\t";
+ 
+ states_array.each_index { |elem_index|
+   states+="\"#{states_array[elem_index]}\" "
+   #don't put an extra ',' on the last array element, although that later C standards hava added support for it.
+   unless elem_index == states_array.length()-1
+     states+=",";
+   end
+}
+ states+="\n};\n";
+ implementation_file+=states;
+ 
  #build the no_tran function
  functions+= "\ntheState no_tran(void){\n\n";
  functions+="\tchar *current_state = \"NO_TRAN_STATE\";\n";
  functions+="\tprintf(\"ACTION: %s, TRIGGERED\\n\", current_state);"; #escape the \n
  functions+="\n\t/*you have entered a non-valid state of the graph. Something weird has happened.*/";
- functions+="\n\texit(0);";
+# functions+="\n\texit(0);";
  functions+="\n}\n";
  
  #build trigger_<> function bodies
@@ -123,15 +144,17 @@ SECTION2
    if key.eql?(states_array.last) then
      functions+= "\ntheState trigger_#{key}(void){\n\n";
      functions+="\tchar *current_state = \"#{key}\";\n";
-     functions+="\tprintf(\"ACTION: %s, TRIGGERED\\n\", current_state);"; #escape the \n
+     functions+="\tprintf(\"STATE: %s, TRIGGERED\\n\", current_state);"; #escape the \n
      functions+="\n\t/*you have entered the last state of the graph.There is nowhere to go from here.*/";
      functions+="\n\texit(0);";
      functions+="\n}\n";     
    else
      functions+= "\ntheState trigger_#{key}(void){\n\n";
-     functions+="\tchar *current_state = \"#{key}\";\n";
-     functions+="\tprintf(\"ACTION: %s, TRIGGERED\\n\", current_state);"; #escape the \n
-     functions+="\n\tuint8_t go_to_state = #{array_val.last}\n";
+#     functions+="\tchar *current_state = \"#{key}\";\n";
+     functions+="\tchar *current_state = (char*)malloc(sizeof(char)*#{key.length});\n";
+     functions+="\tstrcpy(current_state, \"#{key}\");\n";
+     functions+="\tprintf(\"STATE: %s, TRIGGERED\\n\", current_state);"; #escape the \n
+     functions+="\n\tuint8_t go_to_state = #{array_val.last};\n";
      functions+="\n\t/*here you can do whatever you must to go to the next possible states*/";
      functions+="\n\t/*go_to_state = */\n";
      functions+="\n\tswitch(go_to_state)";
@@ -140,6 +163,7 @@ SECTION2
        functions+="\n\t\tcase #{elem}:";
        functions+="\n\t\t\tstrcpy(current_state,\"#{elem}\");"
        functions+="\n\t\t\tprintf(\"GOING TO: %s \\n\", current_state);";
+       functions+="\n\t\t\tfree(current_state);";
        functions+="\n\t\t\treturn #{elem};";
      };
      functions+="\n\t\tdefault:";
@@ -154,6 +178,60 @@ SECTION2
  implementation_file;
 end
 
+#The heredoc 'template' for sample main code file code gen.
+def return_sample_main_file_template(states_array, states_hash)
+
+ states="";
+ functions="";
+ sample_main_code= <<SECTION1
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "fsm.h"
+
+SECTION1
+ 
+ sample_main_code+= <<SECTION2
+/* This is sample code to copy in your main.c file.
+ * The template code enters the first state as configured by your .yml file, 
+ * and calls the relevant trigger_ function.
+ */
+extern theState (* const state_trigger[MAX_STATES+1])(void);\n
+int main(int argc, char** argv) {\n
+SECTION2
+
+# sample_main_code+= "\tuint8_t from_state = #{states_array[0]};\n";
+
+ sample_main_code+= <<SECTION3
+\tuint8_t go_to_state = #{states_array[0]};\n
+\twhile(1)\n\t{
+\t\tswitch(go_to_state)
+\t\t{
+SECTION3
+ 
+ #build trigger_<> function bodies
+ states_hash.each{ |key, array_val|
+   
+#     array_val.each{ |elem|
+       functions+="\t\t\tcase #{key}:\n";
+       functions+="\t\t\t\tgo_to_state = (*state_trigger[go_to_state])();\n"
+       functions+="\t\t\t\tbreak;\n"
+ #    };
+#     functions+="\n\t\t\t\tstrcpy(current_state,\"#{array_val.last}\");"
+ #    functions+="\n\t\t\t\tprintf(\"GOING TO: %s \\n\", current_state);"
+  #   functions+="\n\t\t\t\treturn #{array_val.last};\n\t}";
+ }
+
+ functions+="\t\t\tdefault:\n";
+ functions+="\t\t\t\tprintf(\"Erroneous State\\n\");\n";
+ functions+="\t\t\t\texit(-1);\n";
+ functions+="\t\t}/*switch case ends here*/";
+ functions+="\n\t}/*while loop ends here*/";
+ functions+="\n}";
+ sample_main_code+=functions;
+ sample_main_code;
+end
 
 if ARGV.length() == 0 then
   printf("Missing .yml file to parse state graph input from\n");
@@ -183,14 +261,18 @@ else
     unless Dir.exist?(File.join(Dir.pwd, FILE_GEN_DIR))
       Dir.mkdir(File.join(Dir.pwd, FILE_GEN_DIR), 0777);
     end
-    fsmc = File.new(File.join(Dir.pwd, FILE_GEN_DIR, "gfsm.c"), "w");
+    fsmc = File.new(File.join(Dir.pwd, FILE_GEN_DIR, "fsm.c"), "w");
     fsmc.write(return_implementation_file_template(states_arr,states_hash).to_s);
     fsmc.flush();
     fsmc.close();    
-    fsmh = File.new(File.join(Dir.pwd, FILE_GEN_DIR, "gfsm.h"), "w");
+    fsmh = File.new(File.join(Dir.pwd, FILE_GEN_DIR, "fsm.h"), "w");
     fsmh.write(return_header_file_template(states_arr));
     fsmh.flush();
     fsmh.close();
+    samplemainc = File.new(File.join(Dir.pwd, FILE_GEN_DIR, "samplemain.c"), "w");
+    samplemainc.write(return_sample_main_file_template(states_arr,states_hash).to_s);
+    samplemainc.flush();
+    samplemainc.close();
   rescue
    raise();
   ensure
